eea6fc7ba0301398efd98ba4a8a8f3ab2d722948
Suyash <suyash.krishna9@gmail.com>
2023-09-10 11:18:59 +0400

    Add logs

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 // Define a unique cache name for your app
-const cacheName = 'scsmath-app-cache-v3';
+const cacheName = 'scsmath-app-cache-v3.1';
 
 // List of static assets to cache when the service worker is installed
 const staticAssets = [
@@ -14,8 +14,10 @@ const staticAssets = [
 
 // Install event: Cache static assets when the service worker is installed
 self.addEventListener('install', (event) => {
+  console.log('Service worker installing');
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
+      console.log('Service worker installed');
       return cache.addAll(staticAssets);
     })
   );
@@ -23,8 +25,10 @@ self.addEventListener('install', (event) => {
 
 // Activate event: Remove old caches if a new version of the service worker is activated
 self.addEventListener('activate', (event) => {
+  console.log('Service worker activating');
   event.waitUntil(
     caches.keys().then((cacheNames) => {
+      console.log('Service worker activated');
       return Promise.all(
         cacheNames.filter((name) => name !== cacheName).map((name) => caches.delete(name))
       );
@@ -34,18 +38,22 @@ self.addEventListener('activate', (event) => {
 
 // Fetch event: Intercept and respond to fetch requests
 self.addEventListener('fetch', (event) => {
+  console.log('Service worker fetching');
   event.respondWith(
     // Try to fetch the request from the cache
     caches.match(event.request).then((response) => {
       // If the request is in the cache, return the cached response
       if (response) {
+        console.log('Loading cached version');
         return response;
       }
 
       // If the request is not in the cache, fetch it from the network
       return fetch(event.request).then((networkResponse) => {
+        console.log('Fetching from web');
         // Cache a copy of the response for future use
         caches.open(cacheName).then((cache) => {
+          console.log('Registering new cache');
           cache.put(event.request, networkResponse.clone());
         });
 
