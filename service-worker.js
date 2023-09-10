094603b61ada69939c0775bc8ce301d8c2fe0a5b
Suyash <suyash.krishna9@gmail.com>
2023-09-10 11:10:06 +0400

    sw v3

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,63 @@
 // service-worker.js
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.3.0/workbox-sw.js');
+// Define a unique cache name for your app
+const cacheName = 'scsmath-app-cache-v3';
 
-if (self.location.hostname.includes('scsmathapp.github.io') && workbox) {
-  const cacheName = 'scsmath-app-cache-v2.2';
+// List of static assets to cache when the service worker is installed
+const staticAssets = [
+  // Add paths to your app's static assets here
+  '/index.html',
+  '/build/bundle.js',
+  '/build/bundle.js.map',
+  '/build/bundle.css',
+  '/global.css'
+];
 
-  workbox.precaching.precacheAndRoute([
-    // Add paths to your app's static assets here
-    '/index.html',
-    '/build/bundle.js',
-    '/build/bundle.js.map',
-    '/build/bundle.css',
-    '/global.css'
-  ]);
-
-  // Clean previous version
-  self.addEventListener('activate', (event) => {
-    event.waitUntil(
-      caches.keys().then((cacheNames) => {
-        return Promise.all(
-          cacheNames.filter((cache) => cache !== cacheName).map((cache) => caches.delete(cache))
-        );
-      })
-    );
-  });
-
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        // If the fetch fails (offline), respond with a cached version or an offline message
-        return caches.match(event.request).then((response) => {
-          if (response) {
-            return response;
-          } else {
-            return new Response('You are offline', { status: 503, statusText: 'Service Unavailable' });
-          }
-        });
-      })
-    );
-  });
-  
-
-  // Cache images and PDFs in a specific subfolder
-  workbox.routing.registerRoute(
-    ({ url }) => url.pathname.startsWith('/assets/'),
-    new workbox.strategies.CacheFirst({
-      cacheName: cacheName
+// Install event: Cache static assets when the service worker is installed
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(staticAssets);
     })
   );
+});
 
-  // Additional caching strategies and routes can be configured here
-}
+// Activate event: Remove old caches if a new version of the service worker is activated
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== cacheName).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch event: Intercept and respond to fetch requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    // Try to fetch the request from the cache
+    caches.match(event.request).then((response) => {
+      // If the request is in the cache, return the cached response
+      if (response) {
+        return response;
+      }
+
+      // If the request is not in the cache, fetch it from the network
+      return fetch(event.request).then((networkResponse) => {
+        // Cache a copy of the response for future use
+        caches.open(cacheName).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+        });
+
+        // Return the network response to the page
+        return networkResponse;
+      }).catch((error) => {
+        // Handle fetch errors (e.g., offline) gracefully
+        console.error('Fetch error:', error);
+
+        // You can customize the offline response here
+        return new Response('You are offline.', { status: 503, statusText: 'Service Unavailable' });
+      });
+    })
+  );
+});
